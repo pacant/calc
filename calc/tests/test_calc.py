d99d728a5f61ee1734175faf7afc74def703381b
pacant <pacant2@gmail.com>
2021-10-14 17:42:44 +0200

    first commit

---
+++ calc/tests/test_calc.py
@@ -0,0 +1,33 @@
+import unittest
+import json
+from flask import request, jsonify
+from calc import app
+
+app.testing = True
+
+# TODO: Extend these component tests for the calc view
+#       and THEN implement all 4 operations!
+# DO NOT REMOVE EXISTING TESTS!
+
+
+class TestCalc(unittest.TestCase):
+    def test_sum1(self):
+        tested_app = app.test_client()
+        reply = tested_app.get("/calc/sum?m=3&n=5").get_json()
+        self.assertEqual(reply["result"], "8")
+
+    def test_div1(self):
+        tested_app = app.test_client()
+        reply = tested_app.get("/calc/div?m=3&n=0").get_json()
+        self.assertEqual(reply["result"], "DivisionByZeroError")
+    
+    #new test
+    def test_subtract1(self):
+        tested_app = app.test_client()
+        reply = tested_app.get("/calc/sub?m=3&n=2").get_json()
+        self.assertEqual(reply["result"], "1")
+        
+    def test_multiply1(self):
+        tested_app = app.test_client()
+        reply = tested_app.get("/calc/mul?m=3&n=5").get_json()
+        self.assertEqual(reply["result"], "15")
